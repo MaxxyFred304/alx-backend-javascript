9cd928829cb4a70f369aabd4a2c3e493654b1046
root <maxfred304@gmail.com>
2023-06-29 21:02:37 +0000

    import uploadPhoto and createUser from utils.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,12 @@
 import { uploadPhoto, createUser } from "./utils";
 
-function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then(([photo, user]) => {
-      console.log(`photo-${photo.body} ${user.firstName} ${user.lastName}`);
-    })
-    .catch(() => {
-      console.log("Signup system offline");
-    });
+async function handleProfileSignup() {
+  try {
+    const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
+    console.log(`photo-${photo.body} ${user.firstName} ${user.lastName}`);
+  } catch (error) {
+    console.log("Signup system offline");
+  }
 }
 
 export default handleProfileSignup;
